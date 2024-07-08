3e1ba71ecb9710683ed1ecdf80ac77761c856e48
Oriol <purullurrea@gmail.com>
2024-07-08 18:39:17 +0200

    endedTest

--- food/views.py
+++ food/views.py
@@ -3,7 +3,8 @@ from django.http import HttpResponse
 from .models import Item
 from .forms import ItemForm
 from django.views.generic.list import ListView
-
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 
 # Create your views here.
@@ -36,6 +37,12 @@ def detail(request, item_id):
     }
     return render(request, "food/detail.html", context)
 
+
+class FoodDetail(DetailView):
+    model = Item
+    template_name = 'food/detail.html'
+
+
 def create_item(request):
     form = ItemForm(request.POST or None)
 
@@ -44,6 +51,16 @@ def create_item(request):
         return redirect('food:index')
     
     return render(request, "food/item-form.html", {'form': form})
+
+class CreateItem(CreateView):
+    model = Item
+    fields = ['item_name', 'item_desc', 'item_price', 'item_image']
+    template_name = 'food/item-form.html'
+
+    def form_valid(self, form):
+        form.instance.user_name = self.request.user
+
+        return super().form_valid(form)
 
 def update_item(request, id):
 
